a9e156ce487e6f546f5017f7383726e8f3553324
dallinmoak <dallinmoak@gmail.com>
2018-01-23 14:16:02 -0700

    added delete button with destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,14 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    #this is all redirecting
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'item was successfully destroyed.' }
+    end
+  end
+
 end
