ff9478a0312999cd6833a9b0adb23b8f37d9e487
dallinmoak <dallinmoak@gmail.com>
2018-01-24 14:32:59 -0700

    create topic table w/validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124164838) do
+ActiveRecord::Schema.define(version: 20180124201748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 20180124164838) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_used"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
