c056ca7e0959e6fefe967cf945ac3a97badbc14e
dallinmoak <dallinmoak@gmail.com>
2018-01-25 15:21:46 -0700

    added technology table and gave it a relationship with portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,9 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  has_many :technologies
   include Placeholder
+
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
   def self.angular
     where(subtitle: "Angular")
   end
