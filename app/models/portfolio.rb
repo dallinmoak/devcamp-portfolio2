e381e567cf5b4328241a54b4bc49ba045427dd61
dallinmoak <dallinmoak@gmail.com>
2018-01-25 15:49:31 -0700

    got rid of the custom scope called angular

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,16 +4,6 @@ class Portfolio < ApplicationRecord
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
-  def self.angular
-    where(subtitle: "Angular")
-  end
-
-  def self.ruby_on_rails
-    where(subtitle: "Ruby on Rails")
-  end
-  #alternate syntax:
-  # scope :ruby_on_rails, -> {where(subtitle: "Ruby on Rails")}
-
   after_initialize :set_defaults
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
