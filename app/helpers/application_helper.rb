21c88913168ea417c453de6434a7ec3eaeac5cc9
dallinmoak <dallinmoak@gmail.com>
2018-02-02 11:38:13 -0700

    LOTS of style stuff. like idek

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  def login_helper
+  def login_helper style
     if current_user.class == User
-      link_to "logout", destroy_user_session_path, method: :delete
+      link_to "logout", destroy_user_session_path, method: :delete, class: style
     else
-      (link_to "login", new_user_session_path) +
-      "<br>".html_safe +
-      (link_to "register", new_user_registration_path)
+      (link_to "login", new_user_session_path, class: style) +
+      "".html_safe +
+      (link_to "register", new_user_registration_path, class: style)
     end
   end
 
