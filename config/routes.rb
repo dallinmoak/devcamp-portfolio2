a7ec0080da53f29612e2da52b67c8dfde077d70b
dallinmoak <dallinmoak@gmail.com>
2018-02-13 13:46:27 -0700

    comment socket, alled it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :comments
+  # resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
   resources :portfolios , except: [:show] do
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     end
   end
 
+  mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
