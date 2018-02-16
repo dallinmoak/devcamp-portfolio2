8555a94d4684bc5c6a482211d72c0c8a03df9e16
dallinmoak <dallinmoak@gmail.com>
2018-02-15 23:58:45 -0700

    more font stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 Rails.application.config.assets.precompile += %w( blogs.scss )
 Rails.application.config.assets.precompile += %w( portfolios.scss )
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
